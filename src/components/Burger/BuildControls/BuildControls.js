6c04b338678c3786ea8ba986d93415cb0b230051
SHIJI.js <59438923+Oluwasijibomi@users.noreply.github.com>
2021-04-27 12:05:18 +0200

    order summary, add ingredients, remove ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,7 +3,7 @@ import classes from "./BuildControls.css";
 import BuildControl from "./BuildControl/BuildControl"
 
 const controls = [
-        {labels: "salad", type: "salad"},
+        {labels: "Salad", type: "salad"},
         {labels: "Bacon", type: "bacon"},
         {labels: "Cheese", type: "cheese"},
         {labels: "Meat", type: "meat"}
@@ -12,9 +12,19 @@ const controls = [
  const buildControls = (props) => (
     
         <div className={classes.BuildControls}>
+
+                <p>Current Price: <strong>{props.price.toFixed(2 )}</strong></p>
             {controls.map(ctrl => (
-                    <BuildControl key={ctrl.label} label={ctrl.label}/>
+                    <BuildControl key={ctrl.labels} label={ctrl.labels}  
+                    added={() => props.ingredientAdded(ctrl.type)} 
+                    removed={() => props.ingredientRemoved(ctrl.type)} 
+                    disabled={props.disabled[ctrl.type]}/>
             ))}
+
+            <button 
+            className={classes.OrderButton}
+            disabled={!props.purchaseable}
+            >ORDER NOW</button>
         </div>
     
 )
